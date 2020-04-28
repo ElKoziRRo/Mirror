2265aabf3ab3b59b02cf98c6551c5f960a636e1e
James Frowen <jamesfrowendev@gmail.com>
2020-04-28 15:33:56 +0100

    tests

---
+++ Assets/Mirror/Tests/Editor/ClientSceneTests.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+
+namespace Mirror.Tests
+{
+    public class ClientSceneTests
+    {
+
+
+
+        // old tests below
+
+
+        [Test] public void AppliesPayloadCorrectly() { Assert.Ignore("Not Implemented"); }
+        [Test] public void SpawnsPrefabIfOneExists() { Assert.Ignore("Not Implemented"); }
+        [Test] public void SpawnsObjectFromSpawnHandler() { Assert.Ignore("Not Implemented"); }
+        [Test] public void AppliesPayloadToObjectInSpawnAsync() { Assert.Ignore("Not Implemented"); }
+        [Test] public void SpawnsSceneObject() { Assert.Ignore("Not Implemented"); }
+
+
+        [Test] public void RegisterPrefab() { Assert.Ignore("Not Implemented"); }
+        [Test] public void RegisterPrefabSetsAssetId() { Assert.Ignore("Not Implemented"); }
+        [Test] public void RegisterSpawnDelegate() { Assert.Ignore("Not Implemented"); }
+        [Test] public void RegisterSpawnHandlerDelegate() { Assert.Ignore("Not Implemented"); }
+        [Test] public void RegisterSpawnAsyncHandlerDelegate() { Assert.Ignore("Not Implemented"); }
+
+        [Test] public void RegisterSpawnHandlerGivesErrorIfWrongHandlerAlreadyExists() { Assert.Ignore("Not Implemented"); }
+        [Test] public void RegisterSpawnAsyncHandlerGivesErrorIfWrongHandlerAlreadyExists() { Assert.Ignore("Not Implemented"); }
+
+        [Test] public void UnregisterPrefabRemovesAllHandlers() { Assert.Ignore("Not Implemented"); }
+
+
+
+        [Test] public void GetPrefabReturnsTrueAndPrefab() { Assert.Ignore("Not Implemented"); }
+        [Test] public void GetPrefabReturnsFalseForNoPrefab() { Assert.Ignore("Not Implemented"); }
+    }
+}
